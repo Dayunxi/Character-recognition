359705d4c2599f226cb301c5c3b5e8a4aeb70864
Dayunxi <511533552@qq.com>
2019-03-22 11:10:51 +0800

    add image enhance

--- train_data/generate_char.py
+++ train_data/generate_char.py
@@ -20,25 +20,27 @@ class ImageEnhance(object):
 
     @classmethod
     def add_erode(cls, img):
-        kernel = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))
+        kernel = cv.getStructuringElement(cv.MORPH_RECT, (2, 2))
         img = cv.erode(img, kernel)
         return img
 
     @classmethod
     def add_dilate(cls, img):
-        kernel = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))
+        kernel = cv.getStructuringElement(cv.MORPH_RECT, (2, 2))
         img = cv.dilate(img, kernel)
         return img
 
     def enhance(self, img_list):
         for i in range(len(img_list)):
             im = img_list[i]
-            if self.noise and np.random.random() < 0.5:
-                img_list[i] = self.add_noise(im)
             if self.dilate and np.random.random() < 0.5:
                 img_list[i] = self.add_dilate(im)
-            elif self.erode and np.random.random() < 0.5:
+            elif self.erode:
                 img_list[i] = self.add_erode(im)
+            if self.noise and np.random.random() < 0.5:
+                img_list[i] = self.add_noise(im)
+            # cv.imshow('ll,', img_list[i])
+            # cv.waitKey()
 
 
 def get_primer_gb():
@@ -61,8 +63,8 @@ def font2image(font_style, gb_list):
     height = 32
     canvas_width = 50       # 大一点方便裁剪
     canvas_height = 50
-    max_angle = 10
-    angle_step = 3
+    max_angle = 30
+    angle_step = 2
     image = Image.new('L', (canvas_width, canvas_height), 0)  # L=gray RGB=rgb
     # 创建Font对象:
     font = ImageFont.truetype(font_style, font_size)
@@ -71,32 +73,46 @@ def font2image(font_style, gb_list):
 
     # 输出文字:
     optical_char_list = []
+    char_info_list = []
+    # copy from get_integrate.py
+    amount = [1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     for char, code in gb_list:
         print(char)
         image.paste(0, (0, 0, canvas_width, canvas_height))  # erase
         draw.text((5, 5), char, font=font, fill=255)
-        for angle in range(-max_angle, max_angle+1, angle_step):
+        for i, angle in enumerate(range(-max_angle, max_angle+1, angle_step)):
             optical_char = image.rotate(angle, Image.BILINEAR)   # linear interpolation
             box = optical_char.getbbox()
             optical_char = optical_char.crop(box)
             optical_char = optical_char.resize((width, height), Image.ANTIALIAS)
-            optical_char_list.append((optical_char, char))
+            # optical_char_list.append((optical_char, char))
+            # print(i, amount[i], angle)
+            for _ in range(amount[i]):
+                optical_char_list.append(np.array(optical_char))
+                char_info_list.append(char)
             # cv.imshow(char, np.array(optical_char))
             # cv.waitKey()
-    return optical_char_list
+    return optical_char_list, char_info_list
 
 
-def save_to_local(optical_char_list, font_style):
+def save_to_local(optical_char_list, char_info_list, font_style):
     order = {}
-    for image, char in optical_char_list:
+    for image, char in zip(optical_char_list, char_info_list):
         if char not in order:
             order[char] = 1
         file_dict = 'character/{}/'.format(char)
         file_name = '{}_{}_{}.jpg'.format(char, font_style, order[char])
+        # print(char)
         if not os.path.exists(file_dict):
             os.mkdir(file_dict)
-        image.save(file_dict + file_name)
+        # image.save(file_dict + file_name)
+        # cv.imwrite(file_dict + file_name, image)  # 不支持中文
+        cv.imencode('.jpg', image)[1].tofile(file_dict+file_name)
         order[char] += 1
+    pass
+
+
+def image_combine(optical_char_list):
     pass
 
 
@@ -105,10 +121,11 @@ def main():
     print(gb_list)
     print(len(gb_list))
     font_style_list = ['simhei.ttf', 'simkai.ttf', 'simsun.ttc', 'msyh.ttc', 'STXINWEI.TTF', 'SIMLI.TTF', 'FZSTK.TTF']
-    for style in font_style_list:
+    for style in font_style_list[:]:
         print(style)
-        optical_char_list = font2image(style, gb_list[:2])
-        save_to_local(optical_char_list, style.split('.')[0])
+        optical_char_list, char_info_list = font2image(style, gb_list[:10])
+        ImageEnhance().enhance(optical_char_list)
+        save_to_local(optical_char_list, char_info_list, style.split('.')[0])
 
 
 if __name__ == '__main__':

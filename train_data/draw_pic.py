ba4b9871dbfac57b6a6dcca39ce4f7ead1a08714
Dayunxi <511533552@qq.com>
2019-03-24 21:08:21 +0800

    draw accuracy & loss

---
+++ train_data/draw_pic.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def draw(accuracy, loss, alpha):
+    length = len(loss)
+    x = [10*i for i in range(length)]
+    h1, = plt.plot(x, accuracy, 'r', label='accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Step')
+    plt.twinx()
+    h2, = plt.plot(x, loss, 'b', label='loss')
+    plt.ylabel('Loss')
+    plt.legend(handles=(h1, h2), loc='upper left')
+    plt.title('Accuracy & Loss, Alpha=' + str(alpha))
+
+
+def main():
+    alpha_list = [0.01, 0.001, 0.0005]
+    for i, alpha in enumerate(alpha_list):
+        with open('accuracy_loss_{}.txt'.format(alpha), 'r') as file:
+            plt.subplot(2, 2, i + 1)
+            line = file.readline()
+            acc_list = np.array(line.split(', '), dtype=np.float32)
+            line = file.readline()
+            loss_list = np.array(line.split(', '), dtype=np.float32)
+            draw(acc_list, loss_list, alpha)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
